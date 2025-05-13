66b1a338308b4bb70a02becfe0a9e70d946f9235
AfroDizzzy <davidrhode92@gmail.com>
2025-05-14 02:57:41 +1200

    added page layout for card info

--- src/Components/mainBody/mainBody.tsx
+++ src/Components/mainBody/mainBody.tsx
@@ -1,3 +1,7 @@
+import { ProductDetails } from "./productDetails/productDetails";
+
 export function MainBody() {
-    return (<div className='mainbody h-[85vh]  bg-gray-100'></div>)
+    return (<div className='mainbody h-[85vh]  bg-gray-100'>
+        <ProductDetails></ProductDetails>
+    </div>)
 }
--- src/Components/mainBody/productDetails/productDetails.tsx
+++ src/Components/mainBody/productDetails/productDetails.tsx
@@ -1,7 +1,12 @@
 export function ProductDetails(){
     return(
-        <div className="grid grid-rows-2">
-
+        <div className="grid grid-cols-2 bg-amber-200 h-full">
+            <div className=""> card image</div>
+            <div className="grid grid-rows-3">
+                <div className="row-span-1 bg-amber-300">Card Name</div>
+                <div className="row-span-1 bg-amber-400">Information</div>
+                <div className="row-span-1 bg-amber-500">Price Data</div>
+            </div>
         </div>
     )
 }
