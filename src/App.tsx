6b67c50c1629ee0cf49901ec0fce1278a8dc8d23
AfroDizzzy <davidrhode92@gmail.com>
2025-05-16 00:25:17 +1200

    added working tanstackQuerry for edhrecSearch

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css'
 import { Footer } from './Components/footer/footer'
 import { MainBody } from './Components/mainBody/mainBody'
@@ -5,12 +6,24 @@ import { NavBar } from './Components/navBar/navBar'
 
 function App() {
 
+  // Create a client
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false, // Don't refetch when window regains focus
+        retry: 1,                    // Only retry failed queries once
+      },
+    },
+  });
+
   return (
-    <div className='root-div'>
-    <NavBar/>
-    <MainBody/>
-    <Footer/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='root-div'>
+        <NavBar />
+        <MainBody />
+        <Footer />
+      </div>
+    </QueryClientProvider >
   )
 }
 
---
+++ src/hooks/cardSearchHook.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+
+const fetchEDHRECData = async (term: string) => {
+  if (!term || term.trim() === '') {
+    return [];
+  }
+
+  const searchGetHeaders: Headers = new Headers();
+  searchGetHeaders.append('Content-Type', 'application/json')
+  searchGetHeaders.append('origin', '')
+  searchGetHeaders.append('Referer', '')
+
+  const response = await fetch(`https://edhrec.com/api/typeahead?q=${encodeURIComponent(term)}`, {
+    headers: {
+      'Origin': null,
+      'Referer': null,  
+      'Content-Type': 'application/json',
+    }   
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return response.json();
+};
+export const useEDHRECData  = (searchTerm: string) => {
+  return useQuery({
+    queryKey: ['edhrec', searchTerm],
+    queryFn: () => fetchEDHRECData(searchTerm),
+    enabled: !!searchTerm && searchTerm.trim() !== '',
+    staleTime: 0,
+    refetchOnWindowFocus: false
+  });
+};
