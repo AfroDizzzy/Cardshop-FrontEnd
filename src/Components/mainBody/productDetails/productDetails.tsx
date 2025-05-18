ec8a1ff397056c0c988585175a024285bcf4c26e
AfroDizzzy <davidrhode92@gmail.com>
2025-05-19 10:45:27 +1200

    added bad way to transfer state

--- src/Components/mainBody/productDetails/productDetails.tsx
+++ src/Components/mainBody/productDetails/productDetails.tsx
@@ -1,7 +1,26 @@
-export function ProductDetails(){
-    return(
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { fetchIndividualCardDataFromScryfall } from "../../../hooks/cardDetailsHooks";
+import type { EdhrecSearchResponseObject } from "../../../types/EdhrecSearchResponseObject";
+
+export function ProductDetails() {
+    const dummyData: EdhrecSearchResponseObject = {
+        id: '',
+        image: '',
+        label: '',
+        url: ''
+    }
+    const { data } = useQuery({
+        queryKey: ['IndividualCardDataFromScryfall'],
+        //queryFn: () => fetchIndividualCardDataFromScryfall(dummyData)
+    });
+
+    return (
+
         <div className="grid grid-cols-2 bg-amber-200 h-full">
-            <div className=""> card image</div>
+            <div className="">
+                {/* {data && (() => { console.log(data); return null; })()} */}
+                {/* Render card details here if needed */}
+            </div>
             <div className="grid grid-rows-3">
                 <div className="row-span-1 bg-amber-300">Card Name</div>
                 <div className="row-span-1 bg-amber-400">Information</div>
