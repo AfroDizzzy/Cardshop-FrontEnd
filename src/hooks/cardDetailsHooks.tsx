16b9e6f04acd173f978c85a532fd59dcb502a47a
AfroDizzzy <davidrhode92@gmail.com>
2025-05-19 00:23:27 +1200

    scryfall query

---
+++ src/hooks/cardDetailsHooks.tsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import type { EdhrecSearchResponseObject } from "../types/EdhrecSearchResponseObject";
+import type { ScryfallMTGCard } from "../types/ScryfallObject";
+
+const fetchIndividualCardDataFromScryfall = async (cardUUID: EdhrecSearchResponseObject) => {
+//   if (!cardUUID || cardUUID.trim() === '') {
+//     return [];
+//   }
+  console.warn(cardUUID);
+  console.warn(cardUUID.image.substring(0,36));
+  const response = await fetch(`https://api.scryfall.com/cards/${encodeURIComponent(cardUUID.image.substring(0,36))}`, {
+    headers: {
+      'Origin': '',
+      'Referer': '',  
+      'Content-Type': 'application/json',
+    }   
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const jsonResponse: ScryfallMTGCard = await response.json(); 
+  console.warn(jsonResponse)
+  return jsonResponse;
+};
+export const useFetchIndividualCardDataFromScryfall  = (cardUUID: EdhrecSearchResponseObject) => {
+  return useQuery({
+    queryKey: ['IndividualCardDataFromScryfall', cardUUID],
+    queryFn: () => fetchIndividualCardDataFromScryfall(cardUUID),
+    enabled: cardUUID != null,
+    staleTime: 0,
+    refetchOnWindowFocus: false
+  });
+};
