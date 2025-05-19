1672a52b86f8d60ef998ef5d8272bb2967a89ff3
AfroDizzzy <davidrhode92@gmail.com>
2025-05-20 02:08:52 +1200

    scryfall data is stored inside zustand when fetch complete

--- src/hooks/cardDetailsHooks.tsx
+++ src/hooks/cardDetailsHooks.tsx
@@ -1,9 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import type { EdhrecSearchResponseObject } from "../types/EdhrecSearchResponseObject";
 import type { ScryfallMTGCard } from "../types/ScryfallObject";
+import useScryfallMTGCardStore from "../store/scryfallSeletedCardStore";
 
 export const fetchIndividualCardDataFromScryfall = async (cardUUID: EdhrecSearchResponseObject) => {
-  if(cardUUID.id === '' || cardUUID === undefined || null){return {}}
+  if (!cardUUID.id || cardUUID.id.trim() === '') {
+    return {} as ScryfallMTGCard;
+  }
   console.warn(cardUUID);
   console.warn(cardUUID.image.substring(0,36));
   const response = await fetch(`https://api.scryfall.com/cards/${encodeURIComponent(cardUUID.image.substring(0,36))}`, {
@@ -21,10 +24,19 @@ export const fetchIndividualCardDataFromScryfall = async (cardUUID: EdhrecSearch
   console.warn(jsonResponse)
   return jsonResponse;
 };
+
 export const useFetchIndividualCardDataFromScryfall  = (cardUUID: EdhrecSearchResponseObject) => {
+  const setScryfallMTGCard = useScryfallMTGCardStore(
+    (state) => state.setData
+  ); 
+
   return useQuery({
     queryKey: ['IndividualCardDataFromScryfall', cardUUID],
-    queryFn: () => fetchIndividualCardDataFromScryfall(cardUUID),
+    queryFn: async () => {
+      const response = await fetchIndividualCardDataFromScryfall(cardUUID);
+      setScryfallMTGCard(response); 
+      return response;
+    },
     enabled: cardUUID != null,
     staleTime: 0,
     refetchOnWindowFocus: false
