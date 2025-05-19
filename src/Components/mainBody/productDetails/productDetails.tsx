5aecce0ab9f0b6218cb45bdbb61e05926a9adff7
AfroDizzzy <davidrhode92@gmail.com>
2025-05-20 02:09:05 +1200

    updating the mainbody for scryfall data

--- src/Components/mainBody/productDetails/productDetails.tsx
+++ src/Components/mainBody/productDetails/productDetails.tsx
@@ -1,28 +1,34 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query"
-import { fetchIndividualCardDataFromScryfall } from "../../../hooks/cardDetailsHooks";
-import type { EdhrecSearchResponseObject } from "../../../types/EdhrecSearchResponseObject";
+import useScryfallMTGCardStore from "../../../store/scryfallSeletedCardStore";
+import type { ScryfallMTGCard } from "../../../types/ScryfallObject";
 
 export function ProductDetails() {
-    const dummyData: EdhrecSearchResponseObject = {
-        id: '',
-        image: '',
-        label: '',
-        url: ''
-    }
-    const { data } = useQuery({
-        queryKey: ['IndividualCardDataFromScryfall'],
-        //queryFn: () => fetchIndividualCardDataFromScryfall(dummyData)
-    });
 
+    const data: ScryfallMTGCard = useScryfallMTGCardStore(
+        (state) => state.data
+    )
+
+    console.warn(data)
     return (
 
         <div className="grid grid-cols-2 bg-amber-200 h-full">
-            <div className="">
-                {/* {data && (() => { console.log(data); return null; })()} */}
-                {/* Render card details here if needed */}
+            <div className="row-span-1 h-full">
+                {data && <img
+                    className=""
+                    srcSet=
+                        {data.image_uris && `${data.image_uris.small} 200w,`+
+
+                        `${data.image_uris.normal} 800w,`+
+                        `${data.image_uris.large} 1000w`}
+                    alt={data?.name || "Product image"}
+                />}
             </div>
             <div className="grid grid-rows-3">
-                <div className="row-span-1 bg-amber-300">Card Name</div>
+                <div className="row-span-1 bg-amber-300">
+                    {data &&
+                        <div>
+                            {data.name}
+                        </div>}
+                </div>
                 <div className="row-span-1 bg-amber-400">Information</div>
                 <div className="row-span-1 bg-amber-500">Price Data</div>
             </div>
